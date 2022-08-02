9d25e76f2a5f4e39fecf2025afefee78c9c5b5a1
woominsik <9737638@naver.com>
2022-08-02 21:59:32 +0900

    들어오는 요청에 맞는 매핑정보를 찾는다.

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -4,6 +4,7 @@ import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.Repository;
 import com.ll.exam.annotation.Service;
+import com.ll.exam.util.Ut;
 import org.reflections.Reflections;
 
 import java.util.*;
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -4,6 +4,7 @@ import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.article.repository.ArticleRepository;
 import com.ll.exam.article.service.ArticleService;
 import com.ll.exam.home.controller.HomeController;
+import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
