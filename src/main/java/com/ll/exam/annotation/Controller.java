e6e109aa5cec42a95f6bf8de12618ff4ebb6810f
woominsik <9737638@naver.com>
2022-08-02 10:37:00 +0900

    어노테이션 추가

---
+++ src/main/java/com/ll/exam/annotation/Controller.java
@@ -0,0 +1,12 @@
+package com.ll.exam.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+public @interface Controller {
+
+
+}
--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -1,4 +1,13 @@
 package com.ll.exam.article.controller;
 
+import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.GetMapping;
+
+@Controller
 public class ArticleController {
+
+    @GetMapping("/usr/article/list")
+    public void showList(){
+
+    }
 }
