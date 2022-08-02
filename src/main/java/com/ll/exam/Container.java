c2ce7935f0838ea0c40097918a6f49649bedd6a2
woominsik <9737638@naver.com>
2022-08-02 14:11:21 +0900

    서비스도 컴포넌트로 지정하여 스캔

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -1,39 +1,53 @@
 package com.ll.exam;
 
 import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.Service;
 import com.ll.exam.article.controller.ArticleController;
 import com.ll.exam.home.controller.HomeController;
 import org.reflections.Reflections;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Container {
-    private static Map<Class,Object>objects;
+    private static Map<Class, Object> objects;
 
     static {
         objects = new HashMap<>();
 
+        scanComponents();
+    }
+
+    private static void scanComponents() {
+        scanServices();
+        scanControllers();
+    }
+
+    private static void scanServices() {
         Reflections ref = new Reflections("com.ll.exam");
-        for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)){
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Service.class)) {
             objects.put(cls, Ut.cls.newObj(cls, null));
         }
     }
 
-    public static <T> T getObj(Class<T> cls){
+    private static void scanControllers() {
+        Reflections ref = new Reflections("com.ll.exam");
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
+            objects.put(cls, Ut.cls.newObj(cls, null));
+        }
+    }
+
+    public static <T> T getObj(Class<T> cls) {
         return (T)objects.get(cls);
     }
 
-    public static List<String> getControllerNames(){
+    public static List<String> getControllerNames() {
         List<String> names = new ArrayList<>();
 
         Reflections ref = new Reflections("com.ll.exam");
         for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)) {
-
             String name = cls.getSimpleName(); // HomeController
             name = name.replace("Controller", ""); // Home
             name = Ut.str.decapitalize(name); // home
--- src/main/java/com/ll/exam/article/service/ArticleService.java
+++ src/main/java/com/ll/exam/article/service/ArticleService.java
@@ -1,4 +1,7 @@
 package com.ll.exam.article.service;
 
+import com.ll.exam.annotation.Service;
+
+@Service
 public class ArticleService {
 }
