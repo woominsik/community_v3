5f35b3b525fcbf871e29cd123f8326011cc3076c
woominsik <9737638@naver.com>
2022-08-02 10:26:53 +0900

    articleController와 같이 1개만 필요한 것은 싱글톤으로 작동

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,14 +1,30 @@
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    public void junit_assertThat(){
-        int rs = 10+20;
+    public void junit_assertThat() {
+        int rs = 10 + 20;
 
         assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    public void ioc__articleController() {
+        ArticleController articleController = Container.getArticleController();
+
+        assertThat(articleController).isNotNull();
+    }
+
+    @Test
+    public void ioc__articleController__싱글톤() {
+        ArticleController articleController1 = Container.getArticleController();
+        ArticleController articleController2 = Container.getArticleController();
+
+        assertThat(articleController1).isEqualTo(articleController2);
     }
 }
